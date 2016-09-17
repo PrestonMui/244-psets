dcbe0b144eac08bda2a702c9554d3e0cd8bf475a
Preston Mui <prestonmui@gmail.com>
2016-09-17 11:35:37 -0700

    Fixed score test in ps2q3 part c

--- pset2_q3.m
+++ pset2_q3.m
@@ -1,4 +1,5 @@
 % Pset 2, question 3
+% Group members: Christina Brown, Sam Leone, Peter McCrory, Preston Mui
 
 % Set seed
 rng(1234);
@@ -9,87 +10,95 @@ X = randn(N,1);
 e = randn(N,1);
 Y = (X + 0.1*X.^2 + e) > 0;
 
-% b: estimate misspecified Probit by ML
-beta_con = fminunc(@(b)Q(Y,X,b),[0 0])
+% Part B: estimate misspecified Probit by ML
 
-% calculate standard errors
+	beta_con = fminunc(@(b)Q(Y,X,b),[0 0]);
 
-% fisher information
-Xmatrix = [ones(N,1),X];
-YHat = Xmatrix * beta_con';
-fisher = zeros(2,2);
-for i = 1:N
-	score_i = (Y(i) * normpdf(YHat(i)) / normcdf(YHat(i)) ...
-		- (1 - Y(i)) * normpdf(YHat(i)) / (1 - normcdf(YHat(i))));
-	fisher = fisher + score_i * score_i * Xmatrix(i,:)' * Xmatrix(i,:);
-end
-fisher = fisher / N;
+	% calculate standard errors
 
-% average hessian
-hessian = zeros(2,2);
-for i = 1:N
-	y = Y(i);
-	phi = normpdf(YHat(i));
-	Phi = normcdf(YHat(i));
-	yhat = YHat(i);
+	% score outer product (meat)
+	Xmatrix = [ones(N,1),X];
+	LHat = Xmatrix * beta_con';
+	scoreproduct = zeros(2,2);
+	for i = 1:N
+		score_i = (Y(i) * normpdf(LHat(i)) / normcdf(LHat(i)) ...
+			- (1 - Y(i)) * normpdf(LHat(i)) / (1 - normcdf(LHat(i))));
+		scoreproduct = scoreproduct + score_i * score_i * Xmatrix(i,:)' * Xmatrix(i,:);
+	end
+	scoreproduct = scoreproduct / N;
 
-	firstpart = y * phi * (yhat * Phi + phi) / Phi^2;
-	secondpart = (1 - y) * phi * (phi - yhat * (1 - Phi)) / ((1 - Phi)^2);
-	hessian_i = (firstpart + secondpart) * Xmatrix(i,:)' * Xmatrix(i,:);
-	hessian = hessian + hessian_i;
-end
-hessian = hessian / N;
+	% average hessian (bread)
+	hessian = zeros(2,2);
+	for i = 1:N
+		y = Y(i);
+		phi = normpdf(LHat(i));
+		Phi = normcdf(LHat(i));
 
-% report standard errors
-Omega = (1/sqrt(N)) * inv(hessian) * fisher * inv(hessian);
-Omega(1,1)
-Omega(2,2)
+		firstpart = y * phi * (LHat(i) * Phi + phi) / Phi^2;
+		secondpart = (1 - y) * phi * (phi - LHat(i) * (1 - Phi)) / ((1 - Phi)^2);
+		hessian_i = (firstpart + secondpart) * Xmatrix(i,:)' * Xmatrix(i,:);
+		hessian = hessian + hessian_i;
+	end
+	hessian = hessian / N;
 
+	% report standard errors
+	Omega = (1/sqrt(N)) * inv(hessian) * scoreproduct * inv(hessian);
+	disp(['Coefficients a, b: ' ])
+	disp(beta_con(1))
+	disp(beta_con(2))
+	
+	disp('Standard errors a, b: ')
+	disp(sqrt(Omega(1,1)))
+	disp(sqrt(Omega(2,2)))
+	
 % c: Score test on hypothesis that b_2 = 0
-gen_resid = Y .* normpdf(YHat) ./ normcdf(YHat) - (1 - Y) .* normpdf(YHat) ./ (1 - normcdf(YHat));
+% The following follows Wooldridge, pg. 570
+	
+	ghat = normpdf(LHat);
+	Ghat = normcdf(LHat);
+	
+	% LHS: u_i / sqrt(G * (1 - G))
+	aux_lhs = (Y - Ghat) ./ sqrt(Ghat .* (1 - Ghat));
 
-	% regress generalized residuals on X^2 (no constant)
-	X2 = X.^2;
-	coeff = inv(X2'*X2) * X2' * gen_resid;
-	sigma_2_hat = sum((gen_resid - coeff * X2).^2) / (N - 1);
-	se = inv(X2' * X2) * sigma_2_hat;
-	pvalue_b = 1 - normcdf(coeff / se)
+	% RHS: g_i / sqrt(G * (1 - G)) times x and z
+	aux_RHS = bsxfun(@times,ghat ./ sqrt(Ghat .* (1 - Ghat)),[Xmatrix X.^2]);
 
-% d: Calculate unconstrained model
-beta_unc = fminunc(@(b)Q(Y,X,b),[0 0 0])
+	% Regress, obtain explained sum of squares
+	aux_lhs_hat = aux_RHS * inv(aux_RHS' * aux_RHS) * aux_RHS' * aux_lhs;
+	ESS = (aux_lhs_hat - mean(aux_lhs))' * (aux_lhs_hat - mean(aux_lhs));
 
-Xmatrix = [ones(N,1),X,X.^2];
-YHat = Xmatrix * beta_unc';
-fisher = zeros(3,3);
-for i = 1:N
-	score_i = (Y(i) * normpdf(YHat(i)) / normcdf(YHat(i)) ...
-		- (1 - Y(i)) * normpdf(YHat(i)) / (1 - normcdf(YHat(i))));
-	fisher = fisher + score_i * score_i * Xmatrix(i,:)' * Xmatrix(i,:);
-end
-fisher = fisher / N;
+	% The LM statistic has an asymptotic distribution of Chi squared 1 under the null
+	disp(['LM Statistic: ' num2str(ESS)])
+	disp(['P-value: ' num2str(1-chi2cdf(ESS,1))])
 
-% average (negative) hessian
-hessian = zeros(3,3);
-Phi = normcdf(YHat);
-phi = normpdf(YHat);
+% d: Unconstrained models
 
-for i = 1:N
-	y = Y(i);
-	phi = normpdf(YHat(i));
-	Phi = normcdf(YHat(i));
-	yhat = YHat(i);
+	% Calculate unconstrained coefficients
+	beta_unc = fminunc(@(b)Q(Y,X,b),[0 0 0])
+	Xmatrix = [ones(N,1),X,X.^2];
+	LHat = Xmatrix * beta_unc';
 
-	firstpart = y * phi * (yhat * Phi + phi) / Phi^2;
-	secondpart = (1 - y) * phi * (phi - yhat * (1 - Phi)) / ((1 - Phi)^2);
-	hessian_i = (firstpart + secondpart) * Xmatrix(i,:)' * Xmatrix(i,:);
-	hessian = hessian + hessian_i;
-end
-hessian = hessian / N;
+	% average (negative) hessian
+	hessian = zeros(3,3);
+	Phi = normcdf(LHat);
+	phi = normpdf(LHat);
+
+	for i = 1:N
+		y = Y(i);
+		phi = normpdf(LHat(i));
+		Phi = normcdf(LHat(i));
+
+		firstpart = y * phi * (LHat(i) * Phi + phi) / Phi^2;
+		secondpart = (1 - y) * phi * (phi - LHat(i) * (1 - Phi)) / ((1 - Phi)^2);
+		hessian_i = (firstpart + secondpart) * Xmatrix(i,:)' * Xmatrix(i,:);
+		hessian = hessian + hessian_i;
+	end
+	hessian = hessian / N;
 
 % Wald test
 	g = beta_unc(1,3);
 	gprime = [0 0 1];
-	wald = N * g * inv((gprime * inv(hessian) * gprime')) * g;
-	pvalue_d = 1 - chi2cdf(wald,1)
+	wald = N * g * inv((gprime * (1/sqrt(N)) * inv(hessian) * gprime')) * g;
+	pvalue_d = 1 - chi2cdf(wald,1);
+	disp(['P-value for Wald test on b2 = 0: ', num2str(pvalue_d)])
 	
-% (1/sqrt(N)) * inv(hessian) * fisher * inv(hessian)
